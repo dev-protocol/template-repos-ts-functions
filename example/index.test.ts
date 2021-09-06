6a8842ed22c5bf32954d0f4979ef2ed71796a7d7
0xkenta <kenta0558kiichi614@gmail.com>
2021-09-06 16:03:12 +0200

    change by lint

--- example/index.test.ts
+++ example/index.test.ts
@@ -3,7 +3,7 @@ import func from './index'
 import { Context } from '@azure/functions'
 
 test('Returns `Hello {query.name}` with 200', async (t) => {
-	const res = await func((undefined as unknown) as Context, {
+	const res = await func(undefined as unknown as Context, {
 		query: { name: 'Alice' },
 	})
 	t.is(res.body, 'Hello Alice')
